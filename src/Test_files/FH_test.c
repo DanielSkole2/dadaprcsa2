be9211656358c4b97c4516fab00e4a7862f73c3a
ztg229 <ztg229@alumni.ku.dk>
2025-10-25 01:41:18 +0200

    updated comments

--- src/Test_files/FH_test.c
+++ src/Test_files/FH_test.c
@@ -26,24 +26,24 @@ int main() {
 
     //starting tests
     if (system("./fhistogram-mt test1.txt") == 0) {
-        printf("fhistogram Single file test passed\n");
+        printf("fhistogram-mt Single file test passed\n");
     } 
     else {
-        printf("fhistogram Single file test failed\n");
+        printf("fhistogram-mt Single file test failed\n");
     }
 
     if (system("./fhistogram-mt test1.txt test2.txt test3.txt") == 0) {
-        printf("fhistogram Multiple file test passed\n");
+        printf("fhistogram-mt Multiple file test passed\n");
     }
     else {
-        printf("fhistogram Multiple file test failed\n");
+        printf("fhistogram-mt Multiple file test failed\n");
     }
 
     if (system("./fhistogram-mt -n 2 test1.txt test2.txt test3.txt") == 0) {
-        printf("fhistogram multithread test passed\n");
+        printf("fhistogram-mt multithread test passed\n");
     }
     else {
-        printf("fhistogram multithread test failed\n");
+        printf("fhistogram-mt multithread test failed\n");
     }
 
     return 0;
