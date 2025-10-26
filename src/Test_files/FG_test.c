0ab60641d576397ff735c2bc76a49397c559940d
ztg229 <ztg229@alumni.ku.dk>
2025-10-26 12:55:08 +0100

    changes for tests

--- src/Test_files/FG_test.c
+++ src/Test_files/FG_test.c
@@ -11,33 +11,33 @@ int main() {
     //Making test data
     FILE *fptr;
 
-    fptr = fopen("Tgrep1.txt", "w");
+    fptr = fopen("Test_files/Inputs/Tgrep1.txt", "w");
     fprintf(fptr, "kylling, okse, svin\n");
     fprintf(fptr, "vil gerne have mad\n");
     fprintf(fptr, "mad kunne være godt\n");
     fclose(fptr);
     
-    fptr = fopen("Tgrep2.txt", "w");
+    fptr = fopen("Test_files/Inputs/Tgrep2.txt", "w");
     fprintf(fptr, "kylling\n");
     fprintf(fptr, "der kun kylling\n");
     fprintf(fptr, "kylling er også godt\n");
     fclose(fptr);
 
-    fptr = fopen("Tgrep3.txt", "w");
+    fptr = fopen("Test_files/Inputs/Tgrep3.txt", "w");
     fprintf(fptr, "der ikke mere mad\n");
     fprintf(fptr, "hvordan er alt væk\n");
     fprintf(fptr, "tid til at sulte\n");
     fclose(fptr);
 
     //starting tests
-    if (system("./fauxgrep-mt mad Tgrep1.txt") == 0) {
+    if (system("./fauxgrep-mt -n 2 mad Tgrep1.txt") == 0) {
         printf("fauxgrep-mt Single file search passed\n");
     } 
     else {
         printf("fauxgrep-mt Single file search failed\n");
     }
 
-    if (system("./fauxgrep-mt kylling Tgrep1.txt Tgrep2.txt Tgrep3.txt") == 0) {
+    if (system("./fauxgrep-mt -n 2 kylling Tgrep1.txt Tgrep2.txt Tgrep3.txt") == 0) {
         printf("fauxgrep-mt Multi file search passed\n");
     } 
     else {
@@ -51,7 +51,7 @@ int main() {
         printf("fauxgrep-mt Multithread file search failed\n");
     }
 
-    if (system("./fauxgrep-mt kebab Tgrep1.txt Tgrep2.txt Tgrep3.txt") == 0) {
+    if (system("./fauxgrep-mt -n 2 kebab Tgrep1.txt Tgrep2.txt Tgrep3.txt") == 0) {
         printf("fauxgrep-mt No match search passed\n");
     } 
     else {
